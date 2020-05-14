731fc6644810abd9a54c071fedc5da5b23540692
tul0n <sayeeda.saima@selise.ch>
2020-05-15 04:09:23 +0600

    first commit

---
+++ script/script.js
@@ -0,0 +1,31 @@
+
+function myFunction(){
+    var input= document.getElementById("input").value;
+    var output = document.getElementById("output");
+    var remove_space =input.replace(/\s + /g,' ');
+    var remove_line=remove_space.replace(/[\r\n]+/g, '\n');
+
+    
+        var text = '';
+        var total=0;
+        for (var i = 0; i < remove_line.length; i++) {
+           
+          if(remove_line[i]=="\n"){
+            total=0;
+            text +="\n";
+          }
+          else if(remove_line[i]==' '){
+             total+=1;
+             text += remove_line[i];
+             if (total==10) 
+             {
+                 text +="\n";
+                 total=0;
+             } 
+           }
+           else{
+             text += remove_line[i];
+           }  
+        }    
+     output.innerHTML= text;
+}
